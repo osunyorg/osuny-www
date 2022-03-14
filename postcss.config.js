a985b8756df996554a026dbc3afa911bda8b3413
sebousan <sebousan@gmail.com>
2022-03-14 19:02:11 +0100

    testimonials

--- postcss.config.js
+++ postcss.config.js
@@ -13,6 +13,7 @@ module.exports = {
                     'collapsed',
                     /^dropdown/,
                     /^nav-level-/,
+                    /^splide_/,
                     /^is-/,
                     /^has-/,
                     /^js-/,
